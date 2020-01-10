9dd9c26e7354fa89bfc2987589c8bc04e4142acf
钟海 <417233956@qq.com>
2020-01-10 13:28:51 +0800

    net40的控制台使用

---
+++ net40/ConsoleApp40/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //加载log4net日志配置文件
+            log4net.Config.XmlConfigurator.Configure(log4net.LogManager.CreateRepository("log4net-default-repository"), new System.IO.FileInfo("log4net.config"));
+            var log = log4net.LogManager.GetLogger(typeof(Program));
+            log.Info("启动...");
+
+            WebApplication.ZKDemo.Init();
+
+            log.Info("完成...");
+            Console.ReadKey();
+        }
+    }
+}
